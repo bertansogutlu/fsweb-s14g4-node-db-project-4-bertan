8fa3f15026fb92fcf6fae0ea0ab0086404d6216a
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-02 17:58:04 +0300

    checkTarifId middleware degisiklik yapildi

--- api/tarifler/tarif-middleware.js
+++ api/tarifler/tarif-middleware.js
@@ -2,12 +2,11 @@ const tarifModel = require('./tarif-model');
 
 const checkTarifId = async (req,res,next)=>{
     try {
-        const isExist = await tarifModel.idIleTarifGetir(req.params.id);
-        if(isExist.length === 0){
-            res.status(404).json({message: `${req.params.id} li tarif bulunamadi`})
-        } else{
-            req.tarif = isExist;
+        const tarif = await tarifModel.idIleTarifGetir(req.params.id);
+        if(tarif.length !== 0){
             next()
+        } else{
+            res.status(404).json({message: `${req.params.id} li tarif bulunamadi`})
         }
     } catch (error) {
         next(error)
--- api/tarifler/tarif-router.js
+++ api/tarifler/tarif-router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const middleware = require('./tarif-middleware');
+const tarifModel = require('./tarif-model');
 
-router.get('/:id',middleware.checkTarifId,(req,res,next)=>{
+router.get('/:id',middleware.checkTarifId, async (req,res,next)=>{
     try {
-        res.json(req.tarif)
+        const tarif = await tarifModel.idIleTarifGetir(req.params.id)
+        res.json(tarif)
     } catch (error) {
         next(error)
     }
