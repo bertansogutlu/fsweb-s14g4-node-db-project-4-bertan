eb7e024e5e154ef8a6dbb6a051faf731c6d48461
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-02 03:09:47 +0300

    tarifler seed olusturuldu

---
+++ data/seeds/tarifler01.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex("tarifler").insert({
+    tarif_adi: "Spagetti Bolonez",
+  });
+  await knex("adimlar").insert([
+    {
+      adim_sirasi: 1,
+      adim_talimati: "Büyük bir tencereyi orta ateşe koyun",
+    },
+    {
+      adim_sirasi: 2,
+      adim_talimati: "1 yemek kaşığı zeytinyağı ekleyin",
+    },
+  ]);
+  await knex("icindekiler").insert({
+    icindekiler_adi: "zeytinyağı",
+    miktar: 0.014,
+  });
+  await knex("icindekiler_adimlar").insert({
+    {icindekiler_id:1,adim_id:1},
+    {icindekiler_id:1,adim_id:2}
+  });
+};
